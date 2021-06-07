0f7429563a4dd3b4a7851bf7ebaf039446d76e66
Michael Allison <michael@scatter.nyc>
2021-06-07 14:54:41 -0400

    frame stepped avpro playables

---
+++ MediaPlayerControlAsset.cs
@@ -0,0 +1,53 @@
+﻿// You need to define AVPRO_PACKAGE_TIMELINE manually to use this script
+// We could set up the asmdef to reference the package, but the package doesn't 
+// existing in Unity 2017 etc, and it throws an error due to missing reference
+//#define AVPRO_PACKAGE_TIMELINE
+#if (UNITY_2018_1_OR_NEWER && AVPRO_PACKAGE_TIMELINE)
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using System.Collections.Generic;
+using System.Reflection;
+
+//-----------------------------------------------------------------------------
+// Copyright 2020-2021 RenderHeads Ltd.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace RenderHeads.Media.AVProVideo.Playables
+{
+	[System.Serializable]
+	public class MediaPlayerControlAsset : PlayableAsset
+	{
+		public Object binding { get; set; }
+		//public ExposedReference<MediaPlayer> mediaPlayer;
+
+		public MediaReference mediaReference;
+
+		[Range(0f, 1f)]
+		public float audioVolume = 1f;
+		[Min(0f)]
+		public float startTime = 0.0f;
+		public bool pauseOnEnd = true;
+		public bool frameAccurateSeek = false;
+		public double preloadTime = 0.3;
+
+		public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+		{
+			var playable = ScriptPlayable<MediaPlayerControlBehaviour>.Create(graph);
+
+			var behaviour = playable.GetBehaviour();
+			//behaviour.mediaPlayer = mediaPlayer.Resolve(graph.GetResolver());
+			behaviour.audioVolume = audioVolume;
+			behaviour.pauseOnEnd = pauseOnEnd;
+			behaviour.startTime = (double)startTime;
+			behaviour.mediaReference = mediaReference;
+			behaviour.mediaPlayer = (MediaPlayer)binding;
+			behaviour.frameAccurateSeek = frameAccurateSeek;
+			behaviour.stopRenderCoroutine = behaviour.mediaPlayer.GetType().GetMethod("StopRenderCoroutine", BindingFlags.NonPublic | BindingFlags.Instance);
+			behaviour.preloadTime = preloadTime;
+
+			return playable;
+		}
+	}
+}
+#endif
---
+++ MediaPlayerControlBehaviour.cs
@@ -0,0 +1,141 @@
+﻿// You need to define AVPRO_PACKAGE_TIMELINE manually to use this script
+// We could set up the asmdef to reference the package, but the package doesn't 
+// existing in Unity 2017 etc, and it throws an error due to missing reference
+//#define AVPRO_PACKAGE_TIMELINE
+#if (UNITY_2018_1_OR_NEWER && AVPRO_PACKAGE_TIMELINE)
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Diagnostics;
+
+//-----------------------------------------------------------------------------
+// Copyright 2020-2021 RenderHeads Ltd.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace RenderHeads.Media.AVProVideo.Playables
+{
+    public class MediaPlayerControlBehaviour : PlayableBehaviour
+    {
+        public MediaPlayer mediaPlayer = null;
+
+        public MediaReference mediaReference = null;
+        public float audioVolume = 1f;
+        public double startTime = 0.0;
+        public bool pauseOnEnd = true;
+        public bool frameAccurateSeek = false;
+        public MethodInfo stopRenderCoroutine = null;
+        public double preloadTime = 0.3;
+        private bool _preparing = false;
+
+        void DoSeek(double time)
+        {
+            if (frameAccurateSeek)
+            {
+                mediaPlayer.Control.Seek(startTime + time);
+                WaitForFinishSeek();
+            }
+            else
+            {
+                mediaPlayer.Control.SeekFast(startTime + time);
+            }
+        }
+
+        public void PrepareMedia()
+        {
+            if (mediaPlayer == null) return;
+
+            if (mediaPlayer.MediaOpened || _preparing)
+                return;
+
+            if (mediaPlayer.MediaSource == MediaSource.Reference ? mediaPlayer.MediaReference != null : mediaPlayer.MediaPath.Path != "")
+            {
+                if(mediaReference != null && mediaReference != mediaPlayer.MediaReference)
+                {
+                    mediaPlayer.OpenMedia(mediaReference, false);
+                }
+                else
+                {
+                    mediaPlayer.OpenMedia(mediaPlayer.MediaReference, false);
+                }
+
+                if (frameAccurateSeek) stopRenderCoroutine?.Invoke(mediaPlayer, null);
+
+                _preparing = true;
+            }
+        }
+
+        public void StopMedia()
+        {
+            if(mediaPlayer != null && mediaPlayer.Control != null)
+            {
+                mediaPlayer.Control.Stop();
+            }
+            _preparing = false;
+        }
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            if (mediaPlayer != null)
+            {
+                if (mediaPlayer.Control != null)
+                {
+                    DoSeek(playable.GetTime());
+                }
+               
+                mediaPlayer.Play();
+
+                if (!Application.isPlaying || frameAccurateSeek)
+                    mediaPlayer.Pause();
+            }
+            _preparing = false;
+        }
+
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            if (mediaPlayer != null)
+            {
+                if (pauseOnEnd)
+                {
+                    mediaPlayer.Pause();
+                }
+            }
+            _preparing = false;
+        }
+
+        void WaitForFinishSeek()
+        {
+            Stopwatch timer = Stopwatch.StartNew(); //never get stuck
+            while (mediaPlayer.Control.IsSeeking() && timer.ElapsedMilliseconds < 5000)
+            {
+                mediaPlayer.Player.Update();
+                mediaPlayer.Player.EndUpdate();
+            }
+            mediaPlayer.Player.Render();
+        }
+
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            if (mediaPlayer != null) 
+            {
+                if (mediaPlayer.Control != null)
+                {
+                    if (frameAccurateSeek)
+                    {
+                        mediaPlayer.Control.Seek(startTime + playable.GetTime());
+                        WaitForFinishSeek();
+                    }
+                    else if(!Application.isPlaying)
+                    {
+                        mediaPlayer.Control.SeekFast(startTime + playable.GetTime());
+                        mediaPlayer.Player.Update();
+                        mediaPlayer.Player.EndUpdate();
+                        mediaPlayer.Player.Render();
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
